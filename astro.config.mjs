f04ad5abd05acebfbf9444bad58f24dd4d289705
Isaac Gonzalez <desarrollo.fullstack3@atomicsolutions.mx>
2026-04-08 09:21:06 -0700

    feat: main page with dynamic base routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // La URL base de tu perfil de GitHub
+  site: 'https://isaaca-dev.github.io',
+
+  // El nombre exacto de la carpeta/repositorio de este nuevo proyecto
+  base: '/hyper-sense-core',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
